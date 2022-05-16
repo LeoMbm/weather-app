65524dc4afa31349c418a8a1ebf84a8b92065dec
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-16 12:12:15 +0200

    fix token

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
+
+const token = process.env.TOKEN;
+
 async function getWeather() {
   const req = await fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=Brussels&limit=5&appid=0fe74432f1cac8182f5542fc98fe56e2"
+    "http://api.openweathermap.org/geo/1.0/direct?q=Brussels&limit=5&appid=" +
+      token
   );
 
   const data = await req.json();
