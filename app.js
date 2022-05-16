6e76d6bad3096a72f176f57a68a2f462874cc292
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-16 15:42:00 +0200

    display weather

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+const city = document.querySelector("#location");
+const weatherNow = document.querySelector("#weather-now");
+const celsius = document.querySelector("#celsius");
+const iconW = document.querySelector(".top-day");
 
 // LISTENER
 
@@ -11,12 +15,19 @@ button.addEventListener("click", getWeather);
 async function getWeather(e) {
   e.preventDefault();
   const req = await fetch(
-    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
       input.value +
-      "&limit=5&appid=0fe74432f1cac8182f5542fc98fe56e2"
+      "&appid=67867bed00da52965e33edd641635061"
   );
 
   const data = await req.json();
+  const icon = document.createElement("img");
 
+  city.innerHTML = data["city"]["name"] + "," + " " + data["city"]["country"];
+  weatherNow.innerHTML = data["list"][0]["weather"][0]["description"];
+  celsius.innerHTML =
+    Math.round(data["list"][0]["main"]["temp"] - 273.15) + " C°";
+
+  iconW.appendChild(icon);
   console.log(data);
 }
